132d2fe20bb60c7d7e2fc8944ea991c676ebdf08
nkw6 <nicholaskwu@gmail.com>
2022-05-07 21:42:10 -0400

    fix radio button for quiz_2 questions

--- static/main.js
+++ static/main.js
@@ -28,16 +28,27 @@ function quiz1_submit(id, nextID) {
   // }
   submitAnsw(id, nextID);
 }
-function quiz3_set(user_answ) {
-  result = setAnsw(user_answ);
-  console.log("sound id ==" + user_answ);
-  document.getElementById("myAudio" + user_answ).play();
+function quiz2_submit(id, nextID) {
+  var ele = document.getElementsByName("answer");
+
+  for (i = 0; i < ele.length; i++) {
+    if (ele[i].checked) current_answ = ele[i].value;
+    //document.getElementById("result").innerHTML = "Gender: " + ele[i].value;
+  }
+  // if (current_answ === 0) {
+  //   console.log("please select  your answer");
+  //   answ_error.style.visibility = "visible";
+  //   answ_error_text.textContent = " Please select your answer ";
+  //   return;
+  // }
+  submitAnsw(id, nextID);
 }
 function quiz3_submit(id, nextID) {
   submitAnsw(id, nextID);
   let answ_text = document.getElementById("answ_text");
   answ_text.style.visibility = "visible";
 }
+
 function submitAnsw(id, nextID) {
   // if (current_answ === 0) {
   //   answ_error.style.visibility = "visible";
